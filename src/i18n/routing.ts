cdc1948c5201d535f37694d6fb5123f34ec4d69d
HealthyBuilder <maqiyuan162@163.com>
2025-08-06 08:47:49 +0800

    feat: add zh-ch front-end

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,6 @@
 import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
-  locales: ['en' /*, 'zh-hant'*/],
+  locales: ['en' /*, 'zh-hant' */, 'zh-cn'],
   defaultLocale: 'en'
 });
