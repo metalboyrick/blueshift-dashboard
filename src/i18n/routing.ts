64249288386f7defa1aa915a33adafa6eb8490e7
David Leung <265220+dhl@users.noreply.github.com>
2025-08-13 00:20:29 +0800

    Update routing.ts

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -9,15 +9,6 @@ export const localeNames = {
     vi: "Tiếng Việt"
 };
 
-
-export const localeNames = {
-    en: "English",
-    "zh-CN": "简体中文",
-    "zh-HK": "繁體中文",
-    fr: "Français",
-    id: "Bahasa Indonesia"
-};
-
 export const routing = defineRouting({
   locales: ['en', 'fr', 'id', 'vi' /*, 'zh-HK'*/],
   defaultLocale: 'en'
