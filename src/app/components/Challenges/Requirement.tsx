0e82fdb1882e140e09804cf55a5f1f8f197656f4
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2025-08-23 01:23:50 -0600

    change how the Requirement component renders content to allow for markdown rendering

--- src/app/components/Challenges/Requirement.tsx
+++ src/app/components/Challenges/Requirement.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 interface RequirementProps {
   title: string;
-  description: string;
+  children: React.ReactNode;
 }
 
-export function Requirement({ title, description }: RequirementProps) {
+export function Requirement({ title, children }: RequirementProps) {
   return (
     <div className="flex flex-col gap-y-3">
       <span className="font-medium text-primary">{title}</span>
-      <p className="text-secondary leading-[160%] not-prose">{description}</p>
+      <div className="text-secondary leading-[160%] not-prose">{children}</div>
     </div>
   );
 }
